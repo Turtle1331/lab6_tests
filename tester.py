b34da0a70e7ba595fab585f6c4916fa06226bddb
Turtle1331 <turtle1331@users.github.com>
2020-05-25 19:08:21 -0700

    Refactor to support all available tests

--- tester.py
+++ tester.py
@@ -1,12 +1,10 @@
 import cpu
 import pyrtl
 
-import sys
-import traceback
+import sys, os, traceback
+import base64, json
 import re
 
-TXT_SUFFIX = '.txt'
-S_SUFFIX = '_info.s'
 ASSERT_REGEX = re.compile(r'assert\(\s*sim.inspect_mem\((rf|d_mem)\)\[(-?0[xX][0-9a-fA-F]+|-?\d+)\]\s*==\s*(-?0[xX][0-9a-fA-F]+|-?\d+)(?:\s?([&|^])\s*(-?0[xX][0-9a-fA-F]+|-?\d+))?\s*\)')
 REGISTER_NAMES = ['zero'] + re.findall('..', 'atv0v1a0a1a2a3t0t1t2t3t4t5t6t7s0s1s2s3s4s5s6s7t8t9k0k1gpspfpra')
 
@@ -58,23 +56,58 @@ class DictWatcher(dict):
 
 
 def parse_args(fnames):
-    '''Parse test filenames to extract the base test names.'''
-    test_names = []
+    '''Parse test filenames to extract groups of test files.'''
+    test_groups = []
+    test_names = set()
 
     for fname in fnames:
-        if (fname.endswith(TXT_SUFFIX)):
-            base_name = fname[:-len(TXT_SUFFIX)]
-        elif (fname.endswith(S_SUFFIX)):
-            base_name = fname[:-len(S_SUFFIX)]
-        else:
-            print(f'error: filename {fname!r} does not end in {TXT_SUFFIX} or {S_SUFFIX}, skipping test')
-            continue
-        
-        # Avoid duplicates (so that * works as expected)
-        if base_name not in test_names:
-            test_names.append(base_name)
+        group = None
 
-    return test_names
+        # Try to guess the test format from the filename
+        base, _ , ext = fname.partition('.')
+        if ext not in ('txt', 's'):
+            print(f'skipping {fname!r}')
+            continue
+        if base.endswith('_info'):
+            base = base[:-len('_info')]
+
+        inst_file = f'{base}.txt'
+        info_txt  = f'{base}_info.txt'
+        info_s    = f'{base}_info.s'
+        combined  = f'{base}.s'
+
+        isfile = os.path.isfile
+        if isfile(inst_file) and (isfile(info_txt) or isfile(info_s)):
+            # Two files: one for instructions, one for assembly and assertions
+            group = {'multifile': True,
+                     'name': base,
+                     'inst_file': inst_file,
+                     'info_file': info_txt if isfile(info_txt) else info_s}
+        elif isfile(combined):
+            with open(combined) as f:
+                first_line = f.readlines()[0]
+
+            # Check for comment symbol '#' + base64 for '{"' or "{'"
+            if first_line.startswith('#eyI') or first_line.startswith('#eyc'):
+                group = {'multifile': False,
+                         'name': base,
+                         'file': combined,
+                         'first_line': first_line}
+            else:
+                print(f'error: single file for {base!r} found, expected base64 dict but got {first_line!r}, skipping test')
+                continue
+
+        if group:
+            # Avoid duplicates (so that * works as expected)
+            if base not in test_names:
+                test_names.add(base)
+                test_groups.append(group)
+        else:
+            print(f'error: could not determine test file(s) for {base!r}, skipping test')
+
+
+    print(f'{len(test_groups)} test{"" if len(test_groups) == 1 else "s"} found.')
+    return test_groups
 
 
 def parse_assertion(stmt):
@@ -108,57 +141,80 @@ def parse_assertion(stmt):
             'str': stmt}
 
 
-def read_tests(test_names):
+def read_tests(test_groups):
     '''Read the test files and extract the instructions and assertions.'''
     tests = []
-    for base_name in test_names:
-        txt_fname = base_name + TXT_SUFFIX
-        s_fname = base_name + S_SUFFIX
-
-        # Try opening the .txt file
-        try:
-            with open(txt_fname) as f:
-                txt_contents = f.read()
-        except FileNotFoundError:
-            print(f'error: file {txt_fname!r} not found, skipping test')
-            continue
-
-        # Try opening the .s file
-        try:
-            with open(s_fname) as f:
-                s_contents = f.read()
-        except FileNotFoundError:
-            print(f'error: file {s_fname!r} not found, skipping test')
-            continue
-
+    for group in test_groups:
         # Create the test entry
-        test = {'name': base_name}
-        
-        # Read the bytecode instructions from the .txt file
-        try:
-            instructions = []
-            for line in txt_contents.splitlines():
-                if not line.startswith('#'):
-                    instructions.append(int('0x' + line, 16))
-            test['instructions'] = instructions
-        except ValueError:
-            print(f'error: could not parse instruction {line!r} in {txt_fname}, skipping test')
-            continue
-        if not instructions:
-            print(f'error: no instructions in {txt_fname}, skipping test')
-            continue
+        test = {'name': group['name']}
 
-        # Read the assertions from the .s file
+        # Unpack the group
+        name = group['name']
+        multifile = group['multifile']
+        inst_file = group.get('inst_file', None)
+        info_file = group.get('info_file', group.get('file', None))
+        first_line = group.get('first_line', None)
+        
+        # Read the instructions
+        if multifile:
+            # Read instructions from a separate file
+            with open(inst_file) as f:
+                inst_contents = f.readlines()
+
+            # Instructions are encoded as lines of hex words
+            try:
+                instructions = []
+                for line in inst_contents:
+                    # Skip over comments
+                    if not line.startswith('#'):
+                        instructions.append(int('0x' + line, 16))
+                test['instructions'] = instructions
+            except ValueError:
+                print(f'error: could not parse instruction {line!r} in {inst_file}, skipping test')
+                continue
+            if not instructions:
+                print(f'error: no instructions in {inst_file}, skipping test')
+                continue
+
+        else:
+            # Read instructions from the first line of the test file
+            inst_base64 = first_line[1:]
+            try:
+                inst_bytes = base64.b64decode(inst_base64)
+                inst_json = inst_bytes.decode('utf-8')
+                inst = json.loads(inst_json)
+            except base64.binascii.Error as e:
+                print(f'error: could not decode base64 instructions in {info_file}: {e}')
+                continue
+            except UnicodeError as e:
+                print(f'error: Unicode error from instruction JSON in {info_file}: {e}')
+                continue
+            except json.JSONDecodeError as e:
+                print(f'error: could not decode instruction JSON in {info_file}: {e}')
+                continue
+            
+            # Translate JSON dictionary into list of instructions
+            instructions = [0] * (max(map(int, inst.keys())) + 1)
+            for addr, value in inst.items():
+                instructions[int(addr)] = value
+            test['instructions'] = instructions
+
+
+        # Read the assembly/assertion file
+        with open(info_file) as f:
+            info_contents = f.readlines()
+
+        # Parse the assertions
         assertions = []
-        s_lines = [line.strip() for line in s_contents.splitlines()]
+        s_lines = [line.strip() for line in info_contents]
         for line in s_lines:
-            if line.startswith('#assert('):
-                assertion = parse_assertion(line[1:])
+            if line.startswith('#assert(') or line.startswith('assert('):
+                assertion = parse_assertion(line.lstrip('#'))
                 assertions.append(assertion)
 
         test['assertions'] = assertions
 
-        # Read the assembly instructions from the .s file
+        # Parse the assembly instructions from the .s file
         assembly = []
         section = None
         offset = 0
@@ -167,28 +223,44 @@ def read_tests(test_names):
                 if line.endswith(':'):
                     section = line[:-1].strip()
                     offset = 0
+                elif line.startswith('.'):
+                    pass  # Ignore .text
                 elif not section:
-                    # Necessary due to test2_init.s
-                    pass
+                    pass  # Necessary due to test2_init.s
                 else:
                     assembly.append({'str': line, 'section': section, 'offset': offset})
                     offset += 1
 
         if len(instructions) != len(assembly):
-            print(f'warning: instruction count mismatch between {txt_fname} and {s_fname}: {len(instructions)} != {len(assembly)}')
+            print(f'warning: instruction count mismatch for {name}: {len(instructions)} (machine code) != {len(assembly)} (assembly)')
 
         test['assembly'] = assembly
 
+
+        # Add extra NOP instruction to catch end of execution for programs without an exit loop
+        # addi $zero, $zero, 0
+        test['out_of_bounds'] = len(instructions)
+        instructions.append(0x20000000)
+        assembly.append({'str': 'addi $zero, $zero, 0', 'section': '_out_of_bounds', 'offset': 0})
+
+
         tests.append(test)
 
+
+    if len(tests) == len(test_groups):
+        print('All tests loaded.')
+    else:
+        print(f'{len(tests)} tests loaded.')
+        print(f'warning: {len(test_groups) - len(tests)} tests failed to load')
     return tests
 
 
 def run_test(test):
-    name = test['name']
+    test_name = test['name']
     instructions = test['instructions']
     assembly = test['assembly']
     assertions = test['assertions']
+    out_of_bounds = test['out_of_bounds']
 
 
     # Setup the memory map with initial values and listeners
@@ -220,16 +292,24 @@ def run_test(test):
     pc_since_write = set()
 
     # Run the simulation
-    print(f'Running {test["name"]}')
+    print(f'Running {test_name}')
     try:
         while True:
             try:
                 sim.step({})
             except Exception as e:
+                # If the circuit raises an exception, render the circuit trace
                 sim_trace.render_trace()
                 traceback.print_exc()
                 exit()
+
+            # Check if the program counter has reached the out-of-bounds instruction
             pc = sim.inspect(pc_wire)
+            if pc >= out_of_bounds:
+                print('\npc is past last instruction, exiting program')
+                if pc > out_of_bounds:
+                    print('warning: pc is {pc - out_of_bounds + 1} past last instruction')
+                break
 
             # Print the current program location and instruction
             print(f'\npc: {hex(pc):<8} | {assembly[pc]["section"]} + 0x{assembly[pc]["offset"]:x}')
@@ -262,7 +342,7 @@ def run_test(test):
 
             # Exit the program once the pc has looped with no memory updates
             if pc in pc_since_write:
-                print('loop detected, exiting program')
+                print('\nloop detected, exiting program')
                 break
             if any_writes:
                 pc_since_write.clear()
@@ -306,7 +386,12 @@ def run_test(test):
 
     # Calculate overall results
     if num_valid == 0:
-        print(f'error: no valid assertions')
+        if num_total == 0:
+            print(f'error: no assertions found for {test_name}')
+            print('Please add some assertions!\n')
+        else:
+            print(f'error: no valid assertions for {test_name}')
+            print('Please fix the assertions!\n')
         return
     percentage = (100.0 * num_passed) / num_valid
     num_invalid = num_total - num_valid
@@ -328,9 +413,9 @@ def main():
         exit()
 
     # Read the tests
-    test_files = parse_args(sys.argv[1:])
-    tests = read_tests(test_files)
-    print()
+    test_groups = parse_args(sys.argv[1:])
+    tests = read_tests(test_groups)
+    print('\n')
 
     # Run the tests
     for test in tests:
